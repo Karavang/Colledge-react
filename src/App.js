9b16296909a475a827fc62c0572dc1008ed359ca
Evgeny Shestak <107277013+Karavang@users.noreply.github.com>
2023-09-17 12:28:30 +0200

    first and last

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from "react";
-import Review from "./Review";
+import React, { useState } from "react";
+import SingleQuestion from "./Question";
+import data from "./data";
+
 function App() {
+  const [questions] = useState(data);
+
   return (
     <main>
-      <section className="container">
-        <div className="title">
-          <h2>Our reviews</h2>
-          <div className="underline"></div>
-        </div>
-        <Review />
-      </section>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {questions.map((question) => (
+            <SingleQuestion key={question.id} {...question} />
+          ))}
+        </section>
+      </div>
     </main>
   );
 }
+
 export default App;
